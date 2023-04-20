dc1782005c5c0fe2ffaa3b2c1f64397ca92903d2
Vinícius Batista <112179061+ViBatista0@users.noreply.github.com>
2023-04-20 11:14:12 -0300

    Adicionando o FrontEnd com ReactJS

--- src/main/java/edu/vibatista/controleusuarios/controller/UserController.java
+++ src/main/java/edu/vibatista/controleusuarios/controller/UserController.java
@@ -1,6 +1,9 @@
 package edu.vibatista.controleusuarios.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,4 +22,10 @@ public class UserController {
     User newUser(@RequestBody User newUser){
         return userRepository.save(newUser);
     }
+
+    @GetMapping("/users")
+    List<User> getAllUsers(){
+        return userRepository.findAll();
+    }
+
 }
