ed090dfac4e5a094295224957a1d4319b464f69c
Vinícius Batista <112179061+ViBatista0@users.noreply.github.com>
2023-05-30 12:35:23 -0300

    Back End completo

--- src/main/java/edu/vibatista/controleusuarios/controller/UserController.java
+++ src/main/java/edu/vibatista/controleusuarios/controller/UserController.java
@@ -4,9 +4,11 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -17,24 +19,46 @@ import edu.vibatista.controleusuarios.repository.UserRepository;
 @RestController
 @CrossOrigin("http://localhost:3000")
 public class UserController {
-    
 
     @Autowired
     private UserRepository userRepository;
 
     @PostMapping("/user")
-    User newUser(@RequestBody User newUser){
+    User newUser(@RequestBody User newUser) {
         return userRepository.save(newUser);
     }
 
     @GetMapping("/users")
-    List<User> getAllUsers(){
+    List<User> getAllUsers() {
         return userRepository.findAll();
     }
 
     @GetMapping("/user/{id}")
-    User getUserById(@PathVariable long id){
-        return userRepository.findById(id).orElseThrow(()-> new UserNotFoundException(id));
+    User getUserById(@PathVariable long id) {
+        return userRepository.findById(id).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @PutMapping("/user/{id}")
+    User updateUser(@RequestBody User newUser, @PathVariable long id) {
+        return userRepository.findById(id)
+                .map(user -> {
+                    user.setUsername(newUser.getUsername());
+                    user.setName(newUser.getName());
+                    user.setEmail(newUser.getEmail());
+                    return userRepository.save(user);
+                }).orElseThrow(() -> new UserNotFoundException(id));
+    }
+
+    @DeleteMapping("/user/{id}")
+    String deleteUserById(@PathVariable long id) {
+        if (!userRepository.existsById(id)) {
+            throw new UserNotFoundException(id);
+        }
+
+        userRepository.deleteById(id);
+
+        return "Usuário deletado com sucesso!";
+
     }
 
 }
